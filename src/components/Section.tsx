d4976f824c4d1bd33c22193489b73bf385d6ed7d
Mars4me <lmarsl1994@gmail.com>
2023-02-08 01:35:57 +0200

    done recom section

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -9,7 +9,7 @@ interface SectionProps extends CustomCopmonentProps {
 export const Section = (props: SectionProps) => {
     return (
         <Container className={props.className}>
-            {props.title && <h1 className="text-xl px-6 py-1.5"> {props.title}</h1>}
+            {props.title && <h1 className="px-3 py-2 text-xl"> {props.title}</h1>}
             {props.children}
         </Container>
     );
